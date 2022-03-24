e242c44ec3e61acc0cba8d8d0980fba44dad6b97
Ali <aliabduh.2020@gmail.com>
2022-03-24 14:36:49 +0300

    add style

--- app/views/cart.php
+++ app/views/cart.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="css/cart.css" />
+    <link rel="stylesheet" href="app/views/css/style.css" />
+    <link rel="stylesheet" href="app/views/css/cart.css" />
     <title>cart</title>
 </head>
 <body class="body">
