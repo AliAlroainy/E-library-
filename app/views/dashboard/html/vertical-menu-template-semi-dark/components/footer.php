8c403b625bde6153bb65002e14ce9a83254b7afc
Ali <aliabduh.2020@gmail.com>
2022-03-29 13:16:55 +0300

    add the book page in dashboard

--- app/views/dashboard/html/vertical-menu-template-semi-dark/components/footer.php
+++ app/views/dashboard/html/vertical-menu-template-semi-dark/components/footer.php
@@ -7,7 +7,7 @@
       © <script>
       document.write(new Date().getFullYear())
       </script>
-      , made with ❤️ by <a href="https://themeselection.com/" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
+      , made with ❤️ by <a href="#" class="footer-link fw-bolder">Ali Alroainy</a>
     </div>
     <div>
       
